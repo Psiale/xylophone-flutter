318b500966f55a3a393294f18e914b6a629b05ab
Alexis <psialedev@gmail.com>
2020-11-06 16:58:06 -0600

    added the basics of the app, now I'm going to create branches to work on the features'

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
-class XylophoneApp extends StatelessWidget {
+class XylophoneApp extends StatefulWidget {
+  @override
+  _XylophoneAppState createState() => _XylophoneAppState();
+}
+
+class _XylophoneAppState extends State<XylophoneApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: Container(
+              child: FlatButton(
+                onPressed: () {
+                  soundPlayer('note2.wav');
+                },
+                child: Text('Press me'),
+              ),
+            ),
+          ),
         ),
       ),
     );
+  }
+
+  Widget textBuilder(string) {
+    return Text(string);
+  }
+
+  soundPlayer(song) {
+    final player = AudioCache();
+    player.play(song);
   }
 }
